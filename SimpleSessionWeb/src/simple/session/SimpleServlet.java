06211fbfc4f3500baecbdfc751f517d8cec6b65d
Administrator <Administrator@ip-9-30-144-89.svl.ibm.com>
2016-11-17 07:52:26 -0800

    add SimpleSession

---
+++ SimpleSessionWeb/src/simple/session/SimpleServlet.java
@@ -0,0 +1,59 @@
+package simple.session;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/SimpleServlet")
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SimpleServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int count = 0;
+		System.out.println("request.getSession() >");
+		HttpSession session = request.getSession();
+		System.out.println("request.getSession() <");
+		if(session.getAttribute("count") != null){
+			System.out.println("request.getAttribute() >");
+			count = (Integer) session.getAttribute("count");
+			System.out.println("request.getAttribute() <");
+			System.out.println("request.setAttribute() >");
+			session.setAttribute("count", count+1);
+			System.out.println("request.setAttribute() <");
+		}else{
+			count = 0;
+			System.out.println("request.setAttribute() >");
+			session.setAttribute("count", count+1);
+			System.out.println("request.setAttribute() <");
+		}
+		System.out.println(count);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
